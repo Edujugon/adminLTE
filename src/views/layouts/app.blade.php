fd233fe05df5543f7bd3385aa1d30c9766d93662
Eduardo Marcos <edujugon@hotmail.com>
2016-10-17 17:40:12 +0200

    Update View ------ Linked panel images to adminLTE default images.

--- src/views/layouts/app.blade.php
+++ src/views/layouts/app.blade.php
@@ -175,7 +175,7 @@
                         <div class="box-body chat" id="chat-box">
                             <!-- chat item -->
                             <div class="item">
-                                <img src="dist/img/user4-128x128.jpg" alt="user image" class="online">
+                                <img src="{{asset('adminLTE/dist/img/user4-128x128.jpg')}}" alt="user image" class="online">
 
                                 <p class="message">
                                     <a href="#" class="name">
@@ -202,7 +202,7 @@
                             <!-- /.item -->
                             <!-- chat item -->
                             <div class="item">
-                                <img src="dist/img/user3-128x128.jpg" alt="user image" class="offline">
+                                <img src="{{asset('adminLTE/dist/img/user3-128x128.jpg')}}" alt="user image" class="offline">
 
                                 <p class="message">
                                     <a href="#" class="name">
@@ -217,7 +217,7 @@
                             <!-- /.item -->
                             <!-- chat item -->
                             <div class="item">
-                                <img src="dist/img/user2-160x160.jpg" alt="user image" class="offline">
+                                <img src="{{asset('adminLTE/dist/img/user2-160x160.jpg')}}" alt="user image" class="offline">
 
                                 <p class="message">
                                     <a href="#" class="name">
--- src/views/layouts/header.blade.php
+++ src/views/layouts/header.blade.php
@@ -29,7 +29,7 @@
                                 <li><!-- start message -->
                                     <a href="#">
                                         <div class="pull-left">
-                                            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                                            <img src="{{asset('adminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
                                         </div>
                                         <h4>
                                             Support Team
@@ -42,7 +42,7 @@
                                 <li>
                                     <a href="#">
                                         <div class="pull-left">
-                                            <img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                                            <img src="{{asset('adminLTE/dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                                         </div>
                                         <h4>
                                             AdminLTE Design Team
@@ -54,7 +54,7 @@
                                 <li>
                                     <a href="#">
                                         <div class="pull-left">
-                                            <img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                                            <img src="{{asset('adminLTE/dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
                                         </div>
                                         <h4>
                                             Developers
@@ -66,7 +66,7 @@
                                 <li>
                                     <a href="#">
                                         <div class="pull-left">
-                                            <img src="dist/img/user3-128x128.jpg" class="img-circle" alt="User Image">
+                                            <img src="{{asset('adminLTE/dist/img/user3-128x128.jpg')}}" class="img-circle" alt="User Image">
                                         </div>
                                         <h4>
                                             Sales Department
@@ -78,7 +78,7 @@
                                 <li>
                                     <a href="#">
                                         <div class="pull-left">
-                                            <img src="dist/img/user4-128x128.jpg" class="img-circle" alt="User Image">
+                                            <img src="{{asset('adminLTE/dist/img/user4-128x128.jpg')}}" class="img-circle" alt="User Image">
                                         </div>
                                         <h4>
                                             Reviewers
@@ -164,13 +164,13 @@
                 <!-- User Account: style can be found in dropdown.less -->
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+                        <img src="{{asset('adminLTE/dist/img/user2-160x160.jpg')}}" class="user-image" alt="User Image">
                         <span class="hidden-xs">Alexander Pierce</span>
                     </a>
                     <ul class="dropdown-menu">
                         <!-- User image -->
                         <li class="user-header">
-                            <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                            <img src="{{asset('adminLTE/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
 
                             <p>
                                 Alexander Pierce - Web Developer
